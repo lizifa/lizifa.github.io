64af49226d90746b2d4af3e767e29175e79e3423
lizifa <550083126@qq.com>
2021-04-03 19:49:29 +0800

    'init'

--- custom-service-worker.js
+++
@@ -1,3 +0,0 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js');
-workbox.routing.registerRoute(/\.(?:js|css|html)$/, workbox.strategies.networkFirst());
-workbox.routing.registerRoute(location.href, workbox.strategies.networkFirst());
