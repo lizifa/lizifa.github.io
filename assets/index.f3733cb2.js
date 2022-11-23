84b0ece1cbfd47be1582834c9eb28e2dde432d69
小书包 <550083126@qq.com>
2022-11-23 23:49:28 +0800

    feat:D20221123

---
+++ assets/index.f3733cb2.js
@@ -0,0 +1,5 @@
+import{l as e,_ as o,r as s,o as r,c}from"./index.7ed5a26b.js";const a=`<div style="position:fixed;left:55%;transform:translate(-50%, -50%);text-align:center;top:50%">
+<img src="/src/assets/deadlink-doc.png" style="display: block; width: 300px;">
+<span>\u9875\u9762\u627E\u4E0D\u5230\u4E86 (*>\uFE4F<*)</span>
+</div>
+`,l=e("css_jianjie","404","/notes/404",a),i={setup(){return{markdownContent:l.content}}};function d(p,m,_,t,f,x){const n=s("Article");return r(),c(n,{htmlCode:t.markdownContent},null,8,["htmlCode"])}var u=o(i,[["render",d]]);export{u as default};
